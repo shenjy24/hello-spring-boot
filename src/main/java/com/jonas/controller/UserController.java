9e8333a9a9d211bb463f4d6a940e9b051f104a28
shenjy <shenjy24@gmail.com>
2022-06-08 13:44:35 +0800

    校验

--- src/main/java/com/jonas/controller/UserController.java
+++ src/main/java/com/jonas/controller/UserController.java
@@ -4,10 +4,15 @@ import com.jonas.component.PropertyConfigurer;
 import com.jonas.domain.User;
 import com.jonas.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotBlank;
+
+@Validated
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -15,7 +20,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("getUser")
-    public User getUser() {
+    public User getUser(@RequestBody String userId) {
         return userService.getUser();
     }
 
