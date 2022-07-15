012b4076c08a679acfaaf99950c491a065fe9202
shenjy <shenjy24@gmail.com>
2022-07-15 15:01:56 +0800

    异步优化

--- src/main/java/com/jonas/controller/AsyncController.java
+++ src/main/java/com/jonas/controller/AsyncController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+
 /**
  * @author shenjy
  * @createTime 2022/3/1 15:05
@@ -27,6 +29,20 @@ public class AsyncController {
         log.info("[{}] end time : {}", Thread.currentThread().getName(), System.currentTimeMillis());
     }
 
+    @PostMapping("doAsyncTaskWithThis")
+    public void doAsyncTaskWithThis() {
+        log.info("[{}] start time : {}", Thread.currentThread().getName(), System.currentTimeMillis());
+        asyncService.doAsyncTaskWithThis();
+        log.info("[{}] end time : {}", Thread.currentThread().getName(), System.currentTimeMillis());
+    }
+
+    @PostMapping("doAsyncTaskWithContext")
+    public void doAsyncTaskWithContext() {
+        log.info("[{}] start time : {}", Thread.currentThread().getName(), System.currentTimeMillis());
+        asyncService.doAsyncTaskWithContext();
+        log.info("[{}] end time : {}", Thread.currentThread().getName(), System.currentTimeMillis());
+    }
+
     @PostMapping("doAsyncTask")
     public void doAsyncTask() {
         log.info("[{}] start time : {}", Thread.currentThread().getName(), System.currentTimeMillis());
@@ -38,6 +54,16 @@ public class AsyncController {
     public void doOtherAsyncTask() {
         log.info("[{}] start time : {}", Thread.currentThread().getName(), System.currentTimeMillis());
         asyncService.doOtherAsyncTask();
+        log.info("[{}] end time : {}", Thread.currentThread().getName(), System.currentTimeMillis());
+    }
+
+    @PostMapping("doMultiAsyncTask")
+    public void doMultiAsyncTask() {
+        log.info("[{}] start time : {}", Thread.currentThread().getName(), System.currentTimeMillis());
+        CompletableFuture<String> future1 = asyncService.doAsyncTaskOne();
+        CompletableFuture<String> future2 = asyncService.doAsyncTaskTwo();
+        // 两个异步任务结束后再往下执行
+        CompletableFuture.allOf(future1, future2).join();
         log.info("[{}] end time : {}", Thread.currentThread().getName(), System.currentTimeMillis());
     }
 }
--- src/main/java/com/jonas/service/AsyncService.java
+++ src/main/java/com/jonas/service/AsyncService.java
@@ -1,9 +1,12 @@
 package com.jonas.service;
 
+import com.jonas.component.context.SpringContext;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @author shenjy
@@ -22,7 +25,32 @@ public class AsyncService {
         long start = System.currentTimeMillis();
         Thread.sleep(2000);
         long end = System.currentTimeMillis();
-        log.info("[{}] task cost {} ms", Thread.currentThread().getName(), end - start);
+        log.info("[{}] task1 cost {} ms", Thread.currentThread().getName(), end - start);
+    }
+
+    /**
+     * 同步操作调用异步操作，同一个类中异步不生效
+     */
+    @SneakyThrows
+    public void doAsyncTaskWithThis() {
+        long start = System.currentTimeMillis();
+        Thread.sleep(2000);
+        this.doAsyncTask();
+        long end = System.currentTimeMillis();
+        log.info("[{}] asyncTaskWithThis cost {} ms", Thread.currentThread().getName(), end - start);
+    }
+
+    /**
+     * 同步操作调用异步操作，同一个类中调用需要使用从容器中获取bean，异步才生效
+     */
+    @SneakyThrows
+    public void doAsyncTaskWithContext() {
+        long start = System.currentTimeMillis();
+        Thread.sleep(2000);
+        AsyncService asyncService = SpringContext.getBean(AsyncService.class);
+        asyncService.doAsyncTask();
+        long end = System.currentTimeMillis();
+        log.info("[{}] asyncTaskWithContext cost {} ms", Thread.currentThread().getName(), end - start);
     }
 
     /**
@@ -34,7 +62,7 @@ public class AsyncService {
         long start = System.currentTimeMillis();
         Thread.sleep(2000);
         long end = System.currentTimeMillis();
-        log.info("[{}] task cost {} ms", Thread.currentThread().getName(), end - start);
+        log.info("[{}] asyncTask cost {} ms", Thread.currentThread().getName(), end - start);
     }
 
     /**
@@ -46,6 +74,26 @@ public class AsyncService {
         long start = System.currentTimeMillis();
         Thread.sleep(2000);
         long end = System.currentTimeMillis();
-        log.info("[{}] task cost {} ms", Thread.currentThread().getName(), end - start);
+        log.info("[{}] otherAsyncTask cost {} ms", Thread.currentThread().getName(), end - start);
+    }
+
+    @Async
+    @SneakyThrows
+    public CompletableFuture<String> doAsyncTaskOne() {
+        long start = System.currentTimeMillis();
+        Thread.sleep(2000);
+        long end = System.currentTimeMillis();
+        log.info("[{}] asyncTaskOne cost {} ms", Thread.currentThread().getName(), end - start);
+        return CompletableFuture.completedFuture("异步任务1完成");
+    }
+
+    @Async
+    @SneakyThrows
+    public CompletableFuture<String> doAsyncTaskTwo() {
+        long start = System.currentTimeMillis();
+        Thread.sleep(2000);
+        long end = System.currentTimeMillis();
+        log.info("[{}] asyncTaskTwo cost {} ms", Thread.currentThread().getName(), end - start);
+        return CompletableFuture.completedFuture("异步任务2完成");
     }
 }
