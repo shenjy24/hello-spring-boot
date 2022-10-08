1ff78b79150d71f5bb076752178645feca1e9e8e
shenjy <shenjy24@gmail.com>
2022-10-08 15:11:13 +0800

    ApplicationRunner

---
+++ src/main/java/com/jonas/component/CustomApplicationRunner.java
@@ -0,0 +1,18 @@
+package com.jonas.component;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author shenjy
+ * @createTime 2022/10/8 15:08
+ * @description SpringBoot项目启动时，会自动执行ApplicationRunner中的run方法
+ */
+@Component
+public class CustomApplicationRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(args);
+    }
+}
