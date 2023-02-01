b1e6e5cc40232ad989db9f1f08d29c3c62a9ba07
shenjy <shenjy24@gmail.com>
2023-02-01 20:51:38 +0800

    websocket功能

--- module/src/main/java/com/jonas/module/websocket/server/WebSocketServer.java
+++ module/src/main/java/com/jonas/module/websocket/server/WebSocketServer.java
@@ -1,18 +1,16 @@
 package com.jonas.module.websocket.server;
 
 import com.jonas.module.websocket.config.WebSocketConfigurator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.util.Map;
 
+@Slf4j
 @Component
 @ServerEndpoint(value = "/ws/server", configurator = WebSocketConfigurator.class)
 public class WebSocketServer {
-    private static final Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
 
     /**
      * 连接建立成功调用的方法
@@ -22,7 +20,7 @@ public class WebSocketServer {
         Map<String, Object> userProperties = session.getUserProperties();
         // 获取ip
         String ipAddr = (String) userProperties.get(WebSocketConfigurator.IP_ADDR);
-        logger.info("WS回调打开, ip:{}", ipAddr);
+        log.info("WS回调打开, ip:{}", ipAddr);
     }
 
     /**
@@ -33,7 +31,7 @@ public class WebSocketServer {
         Map<String, Object> userProperties = session.getUserProperties();
         // 获取ip
         String ipAddr = (String) userProperties.get(WebSocketConfigurator.IP_ADDR);
-        logger.info("WS回调关闭, ip:{}", ipAddr);
+        log.info("WS回调关闭, ip:{}", ipAddr);
     }
 
     /**
@@ -44,11 +42,11 @@ public class WebSocketServer {
     @OnMessage
     public void onMessage(String message, Session session) {
         // 收到客户端的消息
-        logger.info("WS回调关闭, message:{}", message);
+        log.info("WS回调关闭, message:{}", message);
     }
 
     @OnError
     public void onError(Session session, Throwable error) {
-        logger.error("WebSocket session:[{}]发生错误，", session.getId(), error);
+        log.error("WebSocket session:[{}]发生错误，", session.getId(), error);
     }
 }
